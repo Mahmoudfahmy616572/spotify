c0a8635cddd415882b38086cce7539dc711b3d89
Mahmoudfahmy616572 <96236826+Mahmoudfahmy616572@users.noreply.github.com>
2025-12-22 19:20:25 +0200

    first commit

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff42C83C);
+  static const backgroundLightTheme = Color(0xffF2F2F2);
+  static const backgroundDarkTheme = Color(0xff121212);
+  static const grey = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xff555555);
+}
