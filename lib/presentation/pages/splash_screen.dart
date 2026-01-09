1de58913b95ae153c0862dc2a34bcbe8a250a539
Mahmoudfahmy616572 <96236826+Mahmoudfahmy616572@users.noreply.github.com>
2026-01-09 15:36:20 +0200

    commit

--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/config/assets/app_vectors.dart';
+import 'package:spotify/presentation/pages/get_started.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    redirect();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,5 +24,11 @@ class SplashScreen extends StatelessWidget {
         child: SvgPicture.asset(AppVectors.logo),
       ),
     );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (BuildContext context) => GetStarted()));
   }
 }
