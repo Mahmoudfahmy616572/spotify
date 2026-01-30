d3df19c0c9c4cc0df9a5968209a7e8eacb52e717
Mahmoudfahmy616572 <96236826+Mahmoudfahmy616572@users.noreply.github.com>
2026-01-30 14:50:39 +0200

    easyyy

--- lib/presentation/songsPlayPage/cubit/cubit/song_player_cubit.dart
+++ lib/presentation/songsPlayPage/cubit/cubit/song_player_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:meta/meta.dart';
-
 part 'song_player_state.dart';
 
 class SongPlayerCubit extends Cubit<SongPlayerState> {
